73b8be6ccf6bc8be9c16a5abf3d1ce5b052417fb
Ruslan Mamaev <ruslan.mamaev@skyeng.ru>
2019-01-31 13:23:59 +0300

    produce separate user outputs

--- templates/org-users.php
+++ templates/org-users.php
@@ -1,10 +1,19 @@
-<?php echo "============== Users list ============== " ?>
+<?php echo "============== Admins list ============== " ?>
 <?php foreach ($org_user_admins as $admin) { ?>
     resource "github_membership" "membership_for_<?= $admin['login'] ?>" {
         username = "<?= $admin['login'] ?>"
         role     = "admin"
     }
 <?php } ?>
+
+<?php echo "============== Admins outputs ============== " ?>
+<?php foreach ($org_user_admins as $admin) { ?>
+    output "github_<?= $admin['login'] ?>_username" {
+        value = "${github_membership.membership_for_<?= $admin['login'] ?>.username}"
+    }
+<?php } ?>
+
+<?php echo "============== Users list ============== " ?>
 <?php foreach ($org_user_members as $user) { ?>
     resource "github_membership" "membership_for_<?= $user['login'] ?>" {
         username = "<?= $user['login'] ?>"
@@ -12,3 +21,9 @@
     }
 <?php } ?>
 
+<?php echo "============== Users outputs ============== " ?>
+<?php foreach ($org_user_members as $user) { ?>
+    output "github_<?= $user['login'] ?>_username" {
+        value = "${github_membership.membership_for_<?= $user['login'] ?>.username}"
+    }
+<?php } ?>
