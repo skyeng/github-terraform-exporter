b4b85a6fc81efc191866154fdab13fbc7bf7da41
Ruslan Mamaev <ruslan.mamaev@skyeng.ru>
2019-02-01 16:44:02 +0300

    fix resource snippet

--- templates/team-members.php
+++ templates/team-members.php
@@ -4,7 +4,7 @@
 foreach ($org_team_members as $team => $users) {
     foreach ($users as $member) { ?>
         resource "github_team_membership" "team_<?= $team ?>_<?= $member['login'] ?>_membership" {
-        team_id  = "${github_team.team_<?= $team ?>.id}"
+        team_id  = "${data.terraform_remote_state.teams.team_<?= $team ?>_id.value}"
         username = "<?= $member['login'] ?>"
         role     = "member"
         }
@@ -15,7 +15,7 @@ foreach ($org_team_members as $team => $users) {
 foreach ($org_team_maintainers as $team => $users) {
     foreach ($users as $maintainer) { ?>
         resource "github_team_membership" "team_<?= $team ?>_<?= $maintainer['login'] ?>_membership" {
-        team_id  = "${github_team.team_<?= $team ?>.id}"
+        team_id  = "${data.terraform_remote_state.teams.team_<?= $team ?>_id.value}"
         username = "<?= $maintainer['login'] ?>"
         role     = "maintainer"
         }
